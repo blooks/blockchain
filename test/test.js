e1ec7d02490bcaa30463d1669200cff31a81241d
Levin Keller <levin@coyno.com>
2015-04-13 16:01:38 +0200

    New chain node version. Tests passing.

--- test/test.js
+++ test/test.js
@@ -11,15 +11,32 @@ var coynochain = new CoynoChain("userId", "sourceId");
 
 // if test function expects second named argument it will be executed
 // in async mode and test will be complete only after callback is called
-describe('coynochain.fetchTransactionsFromAddresses', function() {
-  it('should return transactions', function(done) {
-  coynochain.fetchTransactionsFromAddresses(["1HnhWpkMHMjgt167kvgcPyurMmsCQ2WPgg"], function (err, result) {
-    if (err) {
-      return done(err);
-    }
-    log('data: ' + util.inspect(result, null, 5));
-    result.length.should.be.above(0);
-    done();
+describe('Simple tests', function() {
+  describe('Getting Txs for an address', function () {
+    it('should return transactions', function (done) {
+      coynochain.fetchTransactionsFromAddresses(["1HnhWpkMHMjgt167kvgcPyurMmsCQ2WPgg"], function (err, result) {
+        if (err) {
+          return done(err);
+        }
+        log('data: ' + util.inspect(result, null, 5));
+        result.length.should.be.above(0);
+        done();
+      });
     });
-  })
+  });
+});
+describe('Heavy duty tests', function() {
+  describe('Get Txs for gambling address', function () {
+    it('should return 6323 or more transactions', function (done) {
+      this.timeout(120000);
+      coynochain.fetchTransactionsFromAddresses(["1changePZFvbZivGRy31NNJ21Xf5tPGuV"], function (err, result) {
+        if (err) {
+          return done(err);
+        }
+        log('data: ' + util.inspect(result, null, 5));
+        result.length.should.be.above(6323);
+        done();
+      });
+    });
+  });
 });
