3ffa19d4ed919fbec2748623e3470d4154f18eda
Levin Keller <post@levinkeller.de>
2016-02-24 14:38:40 +0100

    Adding pagination.

--- test/test.js
+++ test/test.js
@@ -14,21 +14,22 @@ var coynochain = new CoynoBlockChainClient("userId", "sourceId");
 describe('Simple tests', function() {
   describe('Getting Txs for an address', function () {
     it('should return transactions', function (done) {
+      this.timeout(5000);
       coynochain.fetchTransactionsFromAddresses(["1HnhWpkMHMjgt167kvgcPyurMmsCQ2WPgg"], function (err, result) {
         if (err) {
           return done(err);
         }
-        log('data: ' + util.inspect(result, null, 5));
-        result.length.should.be.above(0);
+        console.log(result.length);
+        result.length.should.be.equal(37);
         done();
       });
     });
   });
 });
 describe('Heavy duty tests', function() {
-  describe.skip('Get Txs for gambling address', function () {
+  describe('Get Txs for gambling address', function () {
     it('should return 6323 or more transactions', function (done) {
-      this.timeout(120000);
+      this.timeout(1200000);
       coynochain.fetchTransactionsFromAddresses(["1changePZFvbZivGRy31NNJ21Xf5tPGuV"], function (err, result) {
         if (err) {
           return done(err);
