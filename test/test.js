238c2df9ee73b75c071b2d07bb4e7c3c1bd72f4e
Levin Keller <post@levinkeller.de>
2016-08-15 11:45:26 +0200

    Fixing tests. Insight api is weird.

--- test/test.js
+++ test/test.js
@@ -27,7 +27,7 @@ describe('Simple tests', function () {
 })
 describe('Heavy duty tests', function () {
   describe('Get Txs for gambling address', function () {
-    it('should return 6323 or more transactions', function (done) {
+    it('should return 970 or more transactions', function (done) {
       this.timeout(1200000)
       blockchain.fetchTransactionsFromAddresses(['1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX'], function (err, result) {
         if (err) {
